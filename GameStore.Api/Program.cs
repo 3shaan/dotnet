1650cfc46aa5fc8bfb9627d06c01b88b4b49b651
Eshan <3shaan.dev@proton.me>
2026-06-23 14:38:50 +0600

    Add validation attributes to CreateGameDto and configure validation services

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,9 @@
 using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddValidation();
+
 var app = builder.Build();
 
 
--- GameStore.Api/dtos/CreateGameDto.cs
+++ GameStore.Api/dtos/CreateGameDto.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Dtos;
 
 public record CreateGameDto(
-    string Name,
-    string Genre,
-    decimal Price,
+    [Required][StringLength(50)] string Name,
+    [MinLength(2)][StringLength(20)] string Genre,
+    [Range(1, 100)] decimal Price,
     DateOnly Date
 );
